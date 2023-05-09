e3c154f789ebca8b82b0beaf28d4ee65824d3d16
TecnoMayte <114059096+TecnoMayte@users.noreply.github.com>
2023-05-09 03:22:51 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,27 +9,31 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     if (contraseña == entrada) {
         basic.showIcon(IconNames.Yes)
+        music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
         basic.pause(500)
         basic.clearScreen()
-        entrada = ""
     } else {
-        basic.showIcon(IconNames.No)
-        basic.pause(500)
-        basic.clearScreen()
-        entrada = ""
+        if (contraseña != entrada) {
+            contar += 1
+            basic.showIcon(IconNames.No)
+            basic.clearScreen()
+            if (contar == 3) {
+                music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+                basic.showString("BLOQUEADO")
+                basic.pause(500)
+                basic.clearScreen()
+            }
+        }
     }
+    entrada = ""
 })
 // pulsar digito de la contraseña
 input.onButtonPressed(Button.B, function () {
     entrada = "" + entrada + "B"
 })
-// recive señal de warning
-// 
-radio.onReceivedMessage(RadioMessage.message1, function () {
-    basic.showString("WARNING")
-})
 // al iniciar establacemos las variables, la contraseña y el servo
 // 
+let contar = 0
 let entrada = ""
 let contraseña = ""
 contraseña = "ABBA"
@@ -40,8 +44,5 @@ basic.forever(function () {
 // si la contraseña se introduce mal 3 veces ademas de bloquearse manda una seña por radio de warning a otra microbit
 // 
 basic.forever(function () {
-    let contar = 0
-    if (contar == 3) {
-        radio.sendMessage(RadioMessage.message1)
-    }
+	
 })
